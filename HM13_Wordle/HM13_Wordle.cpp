b5f63f5b218a1513c90de2a36fbbb9387ff3efac
Gallifrey <alex.megakep@gmail.com>
2025-01-24 18:10:38 +0100

    concept

---
+++ HM13_Wordle/HM13_Wordle.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using std::cout;
+using std::cin;
+using std::endl;
+
+//init game params
+	//Player input
+	//"Random word" from database
+		//func for random
+	//"Word of a day"
+		//TO DO
+
+constexpr int SIZE = 5;
+
+void randomWord(std::string database[SIZE], std::string mysteryWord[SIZE])
+{
+	//TO DO
+}
+
+void playerGuess(std::string playerInput[SIZE], std::string mysteryWord[SIZE])
+{
+	//TO DO
+}
+
+
+
+int main()
+{
+	int gameMode = -1;
+	std::string database[10] = { "apple", "bread", "chair", "dream", "flute", "grape", "heart", "jelly", "knife", "plant" };
+	std::string playerInput;
+	//std::getline(std::cin, playerInput);
+	
+	//Intro "Hi it is my super game...." & Selection of game mode 
+			//"Random word" +-
+			//"Word of a day"
+			//Close Game
+			// 
+	//Game Loop after GameMode Selected
+			//User trying to guess a Word
+			//Search for common letters in a generated Word
+			//Higlight a right letters
+				//small copy if letter present in a Word
+				//big letter if letter is located on a right position
+	while (gameMode != 0)
+	{
+		
+		cout << "1 - Word of the day" << endl;
+		cout << "2 - Random Word" << endl;
+		cout << "0 - Exit" << endl;
+		cout << "Enter: ";
+		cin >> gameMode;
+
+		if (gameMode == 1)
+		{
+			//"Word of a day"
+			cout << "yes";
+		}
+		else
+		{
+			cout << "no";
+			//"Random word" 
+			//TO DO in a future
+		}
+	}
+
+
+
+
+
+}
