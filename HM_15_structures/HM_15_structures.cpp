73699844372d190f454ebc6169a26d230cf8e613
Gallifrey <alex.megakep@gmail.com>
2025-02-04 22:44:01 +0100

    small changes in main part and 5 task

--- HM_15_structures/HM_15_structures.cpp
+++ HM_15_structures/HM_15_structures.cpp
@@ -11,7 +11,7 @@ struct Student
 
 
 //task 1
-int AverageMark(const Student& student) 
+ double getAverageMark(const Student& student) 
 {
     int size = sizeof(student.marks) / sizeof(student.marks[0]);
     int total = 0;
@@ -22,13 +22,13 @@ int AverageMark(const Student& student)
     return total / size;
 };
 
-void PrintStudentInfo(const Student* student, int studentIndex)
+void printStudentInfo(const Student* student, int studentGroupIndex)
 {
-    std::cout << student[studentIndex].name << " average mark - " << AverageMark(student[studentIndex]) << std::endl;
+    std::cout << student[studentGroupIndex].name << " average mark - " << getAverageMark(student[studentGroupIndex]) << std::endl;
 };
 
 //Task 3
-const Student* GetStudentWithHighScore(const Student* studentPtr, unsigned size)
+const Student* getStudentWithHighScore(const Student* studentPtr, unsigned size)
 {
     int* total = new int[size](); 
 
@@ -54,7 +54,7 @@ const Student* GetStudentWithHighScore(const Student* studentPtr, unsigned size)
 };
 
 //Task 4
-int MarkPredicate(const Student* studentsPtr, unsigned size, int& markPredicate)
+int getStudentsWithMark(const Student* studentsPtr, unsigned size, int& markPredicate)
 {
     int count = 0;
     for (unsigned int i = 0; i < size; i++)
@@ -75,13 +75,51 @@ int MarkPredicate(const Student* studentsPtr, unsigned size, int& markPredicate)
 };
 
 //Task 5 
-void GetBestStudents(Student* inStudents, unsigned inSize, Student* outStudents, unsigned& outSize, unsigned percent) 
+void getOutSize(unsigned int& outSize, const unsigned inSize, unsigned int percent)
 {
-
+    outSize = (percent * inSize) / 100;
+    if (outSize < 1)
+    {
+        outSize = 1;
+    }
 };
 
+void getBestStudents(Student* inStudents, unsigned inSize, Student* outStudents, unsigned& outSize)
+{
+    if (outSize != 0)
+    {
+        double* averages = new double[inSize];
+        int* stud_index = new int[inSize];
+        for (unsigned int i = 0; i < inSize; i++)
+        {
+            averages[i] = getAverageMark(inStudents[i]);
+            stud_index[i] = i;
+        }
+
+        for (unsigned int i = 0; i < inSize; i++)
+        {
+            for (unsigned int j = 0; j < inSize - 1; j++)
+            {
+                if (averages[stud_index[j]] < averages[stud_index[j + 1]])
+                {
+                    std::swap(stud_index[j], stud_index[j + 1]);
+                }
+            }
+        }
+
+        for (unsigned int i = 0; i < outSize; i++)
+        {
+            outStudents[i] = inStudents[stud_index[i]];
+        }
+
+        delete[] averages;
+        delete[] stud_index;
+    }
+}
+
+
 //Task 6
-void SortStudentsByMark(Student* students, unsigned groupSize)
+void sortStudentsByMark(Student* students, unsigned groupSize)
 {
     int* avrMarks = new int[groupSize]();
     for (unsigned int i = 0; i < groupSize; i++)
@@ -94,7 +132,7 @@ void SortStudentsByMark(Student* students, unsigned groupSize)
         avrMarks[i] = avrMarks[i] / markAmount;
     }
 
-    for (unsigned int k = 0 ; k < groupSize; k++) // sorting DOEST NOT WORK  !!!
+    for (unsigned int k = 0; k < groupSize; k++) // sorting DOEST NOT WORK  !!!
     {
         int highest = 0;
         if (avrMarks[k] > avrMarks[highest])
@@ -111,7 +149,7 @@ int main()
     //Task 1
     Student student_1 = { "Jonh", {80, 50, 20, 30} };
     std::cout << "Task 1:" << std::endl;
-    std::cout << student_1.name << " average mark is: " << AverageMark(student_1) << std::endl << std::endl << std::endl;
+    std::cout << student_1.name << " average mark is: " << getAverageMark(student_1) << std::endl << std::endl << std::endl;
     
     //Task 2
     Student groupOfStudents[4] =
@@ -125,28 +163,48 @@ int main()
     //Task 3
     std::cout << "Task 3:" << std::endl;
     
-    const Student* bestStudent = GetStudentWithHighScore(groupOfStudents, 4);
+    const Student* bestStudent = getStudentWithHighScore(groupOfStudents, 4);
     std::cout << "Best student in group is " << bestStudent->name << std::endl;
-    PrintStudentInfo(groupOfStudents, 0);
-    PrintStudentInfo(groupOfStudents, 1);
-    PrintStudentInfo(groupOfStudents, 2);
-    PrintStudentInfo(groupOfStudents, 3);
+    for (int i = 0; i < 4; i++)
+    {
+        printStudentInfo(groupOfStudents, i);
+    }
     
     //Task 4
     int predicate = 80;
     std::cout << "\n\nTask 4:" << std::endl;
-    std::cout << "Amout of student with  mark >= " << predicate << ", is: " << MarkPredicate(groupOfStudents, 4, predicate) << std::endl;
+    std::cout << "Amout of student with  mark >= " << predicate << ", is: " << getStudentsWithMark(groupOfStudents, 4, predicate) << std::endl << std::endl;
 
     //Task 5
     std::cout << "Task 5:" << std::endl;
-    Student perspectiveStudents[4];
+    int percent = 34;
+    unsigned int outSize = 0;
+    const int inSize = 8;
+    Student inStudents[inSize] =
+    {
+    { {"Raphael"}, {80, 85, 70, 65} },
+    { {"Donatello"}, {98, 100, 95, 95} },
+    { {"Leonardo"}, {80, 78, 80, 90} },
+    { {"Michelangelo "}, {90, 68, 61, 78} },
+    { {"Splinter "}, {85, 78, 61, 78} },
+    { {"April "}, {90, 69, 61, 61} },
+    { {"Nikolas"}, {60, 65, 100, 78} },
+    { {"Creng "}, {72, 68, 61, 78} },
+    };
+    getOutSize(outSize, inSize, percent);
+    Student* outStudents = new Student[outSize];
+    getBestStudents(inStudents, inSize, outStudents, outSize);
+    for (unsigned int i = 0; i < outSize; i++)
+    {
+        printStudentInfo(outStudents, i);
+    }
+    delete[] outStudents;
 
     //Task 6
     std::cout << "Task 6:" << std::endl;
-    SortStudentsByMark(groupOfStudents, 4);
-    PrintStudentInfo(groupOfStudents, 0);
-    PrintStudentInfo(groupOfStudents, 1);
-    PrintStudentInfo(groupOfStudents, 2);
-    PrintStudentInfo(groupOfStudents, 3);
+ /*   for (unsigned int i = 0; i < inSize; i++)
+    {
+        PrintStudentInfo(inStudents, i);
+    }*/
 }   
 
