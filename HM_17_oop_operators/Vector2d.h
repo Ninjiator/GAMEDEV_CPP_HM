6be3330c32bf437ecede8e5ee77f2b021a8e7968
Gallifrey <alex.megakep@gmail.com>
2025-02-12 16:09:29 +0100

    add counter in default c-tor

--- HM_17_oop_operators/Vector2d.h
+++ HM_17_oop_operators/Vector2d.h
@@ -5,7 +5,7 @@ class Vector2d
 {
 public:
 	Vector2d(float x, float y);
-	//Vector2d() { std::cout << " [C-tor called] " << std::endl; }
+	Vector2d() { CalledVectorConstructors++; std::cout << " [C-tor called] " << std::endl; }
 
 	static void printCalledVectors() { std::cout << "Vector2d was called: " << CalledVectorConstructors << " times" << std::endl; }
 
@@ -15,7 +15,7 @@ public:
 
 	Vector2d operator+(const Vector2d& secondVector) const {return Vector2d(this->m_x + secondVector.m_x, this->m_y + secondVector.m_y);}
 	Vector2d operator-(const Vector2d& secondVector) const {return Vector2d(this->m_x - secondVector.m_x, this->m_y - secondVector.m_y);}
-	Vector2d operator=(const Vector2d& secondVector) { return Vector2d(this->m_x = secondVector.m_x, this->m_y = secondVector.m_y); }
+	Vector2d operator=(const Vector2d secondVector) { return Vector2d(this->m_x = secondVector.m_x, this->m_y = secondVector.m_y); }
 	float& operator[](std::size_t idx) { if (idx == 0) return this->m_x; else return this->m_y; }
 	
 
