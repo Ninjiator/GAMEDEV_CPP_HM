0e680d8c1ff96d7d295d69ce887c13a70512f3af
Gallifrey <alex.megakep@gmail.com>
2025-02-23 19:15:53 +0100

    tasks completed

---
+++ HM_19_DynamicIntAray/DynamicIntArray.cpp
@@ -0,0 +1,141 @@
+#include "DynamicIntArray.h"
+#include <iostream>
+//Implementations go here
+
+DynamicIntArray::DynamicIntArray() : m_size(0)
+{
+	m_data = new int[m_size];
+}
+
+DynamicIntArray::DynamicIntArray(std::size_t size) : m_size(static_cast<int>(size))
+{
+	if (m_size > 0)
+	{
+		m_data = new int[m_size];
+		for (int i = 0; i < m_size; i++)
+		{
+			m_data[i] = 0;
+		}
+	}
+	else
+	{
+		m_data = nullptr;
+	}
+}
+
+DynamicIntArray::DynamicIntArray(const DynamicIntArray& other)
+{
+	m_data = new int[other.m_size];
+	this->m_size = other.m_size;
+	for (int i = 0; i < other.m_size; i++)
+	{
+		m_data[i] = other.m_data[i];
+	}
+}
+
+DynamicIntArray& DynamicIntArray::operator=(const DynamicIntArray& other)
+{
+	delete[] m_data;
+	m_data = new int[other.m_size];
+	this->m_size = other.m_size;
+	for (int i = 0; i < other.m_size; i++)
+	{
+		m_data[i] = other.m_data[i];
+	}
+	return *this;
+}
+
+void DynamicIntArray::setSize(std::size_t newSize)
+{
+	if (newSize == m_size)
+	{
+		return;
+	}
+
+	int* copy = new int[newSize];
+	if (newSize > m_size)
+	{
+		for (int i = 0; i < m_size; i++)
+		{
+			copy[i] = m_data[i];
+		}
+		for (int j = m_size; j < newSize; j++)
+		{
+			copy[j] = 0;
+		}
+	}
+	else if (newSize < m_size)
+	{
+		for (int i = 0; i < newSize; i++)
+		{
+			copy[i] = m_data[i];
+		}
+	}
+
+	delete[] m_data;
+	m_size = newSize;
+	m_data = copy;
+}
+
+void DynamicIntArray::push_back(int element)
+{
+	int newSize = m_size + 1;
+	this->setSize(newSize);
+	m_data[m_size - 1] = element;
+}
+
+DynamicIntArray::~DynamicIntArray()
+{
+	delete[] m_data;
+}
+
+int& DynamicIntArray::operator[](std::size_t index)
+{
+	return m_data[index];
+}
+
+void DynamicIntArray::clear()
+{
+	delete[] m_data;	
+	m_size = 0;
+	m_data = new int[m_size];
+}
+
+bool DynamicIntArray::operator==(const DynamicIntArray& other)
+{
+	if (this->m_size == other.m_size)
+	{
+		for (int i = 0; i < m_size; i++)
+		{
+			if (this->m_data[i] != other.m_data[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
+}
+
+bool DynamicIntArray::operator!=(const DynamicIntArray& other)
+{
+	if (this->m_size == other.m_size)
+	{
+		for (int i = 0; i < m_size; i++)
+		{
+			if (this->m_data[i] != other.m_data[i])
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	return true;
+}
+
+std::size_t DynamicIntArray::getSize() const
+{
+	return m_size;
+}
+
+
---
+++ HM_19_DynamicIntAray/HM_19_DynamicIntAray.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "DynamicIntArray.h"
+
+int main()
+{
+	DynamicIntArray array1;
+	DynamicIntArray array2(5);
+	std::cout << "Array 1 size: " << array1.getSize() << std::endl;
+	std::cout << "Array 2 size: " << array2.getSize() << std::endl;
+	for (int i = 0; i < array2.getSize(); i++)
+	{
+		array2[i] = 13;
+	}
+	std::cout << "Second element of array 2 is : " << array2[2] << std::endl;
+	array2.clear();
+	std::cout << "Array 2 size after .clear() : " << array2.getSize() << std::endl;
+
+	if (array1 == array2)
+	{
+		std::cout << "true\n";
+	}
+	else
+	{
+		std::cout << "false\n";
+	}
+	DynamicIntArray array3(5);
+	DynamicIntArray array4(4);
+	array3[2] = 13;
+	array4[2] = 13;
+	if (array3 == array4)
+	{
+		std::cout << "Is array 3 == array 4 :\ntrue\n";
+	}
+	else
+	{
+		std::cout << "Is array 3 == array 4 :\nfalse\n";
+	}
+
+	array2 = array4;
+	if (array2 == array4)
+	{
+		std::cout << "Is array 3 == array 4 :\ntrue\n";
+	}
+	else
+	{
+		std::cout << "Is array 3 == array 4 :\nfalse\n";
+	}
+
+	array2.setSize(7);
+	array4.setSize(6);
+	std::cout << "Array 2 size after .setSize(7) : " << array2.getSize() << std::endl;
+	std::cout << "Array 4 size after .setSize(3) : " << array4.getSize() << std::endl;
+	array4.push_back(6);
+	std::cout << "Array 4 size after .push_back(5) : [" << array4.getSize() << "] 5th element is: [" << array4[6] << "]" << std::endl;
+}
