a1c6c90370af149e2a8415d21f8b07b4a13db87f
Gallifrey <alex.megakep@gmail.com>
2025-03-05 19:44:04 +0100

    template class created

--- HM23_DynamicArray/DynamicIntArray.h
+++ HM23_DynamicArray/DynamicIntArray.h
@@ -1,31 +1,166 @@
 #pragma once
 #include <cstdlib>
 
-class DynamicIntArray
+template<typename T>
+class DynamicArray
 {
 public:
-    DynamicIntArray();
+    DynamicArray<T>();
 
-    DynamicIntArray(std::size_t size);
+    DynamicArray<T>(std::size_t size);
+    DynamicArray<T>(const DynamicArray<T>& other);
 
-    ~DynamicIntArray();
+    ~DynamicArray<T>();
 
-    int& operator[](std::size_t index);
+	T& operator[](std::size_t index);
 
     void clear();
 
-    bool operator==(const DynamicIntArray& other);
-    bool operator!=(const DynamicIntArray& other);
+    bool operator==(const DynamicArray& other);
+    bool operator!=(const DynamicArray& other);
 
     std::size_t getSize() const;
 
-    DynamicIntArray(const DynamicIntArray& other);
-    DynamicIntArray& operator=(const DynamicIntArray& other);
+    
+    DynamicArray& operator=(const DynamicArray& other);
     void setSize(std::size_t newSize);
 
-    void push_back(int element);
+    void push_back(T element);
 
 private:
-    int* m_data = nullptr;
+    T* m_data = nullptr;
     int m_size = 0;
 };
+
+
+
+template<typename T>
+DynamicArray<T>::DynamicArray() : m_size(0)
+{
+	m_data = new T[m_size];
+}
+
+template<typename T>
+DynamicArray<T>::DynamicArray(std::size_t size) : m_size(static_cast<int>(size))
+{
+	if (m_size > 0)
+	{
+		m_data = new T[m_size]();
+	}
+	else
+	{
+		m_data = nullptr;
+	}
+}
+
+template<typename T>
+DynamicArray<T>::DynamicArray(const DynamicArray<T>& other)
+{
+	m_data = new T[other.m_size];
+	this->m_size = other.m_size;
+	for (int i = 0; i < other.m_size; i++)
+	{
+		m_data[i] = other.m_data[i];
+	}
+}
+template<typename T>
+DynamicArray<T>& DynamicArray<T>::operator=(const DynamicArray<T>& other)
+{
+	if (this == &other)
+		return *this;
+
+	delete[] m_data;
+
+	m_data = new T[other.m_size];
+
+	m_size = other.m_size;
+	for (int i = 0; i < other.m_size; i++)
+	{
+		m_data[i] = other.m_data[i];
+	}
+
+	return *this;
+}
+
+template<typename T>
+void DynamicArray<T>::setSize(std::size_t newSize)
+{
+	if (newSize == m_size)
+	{
+		return;
+	}
+
+	T* copy = new T[newSize];
+	int copySize = std::min(static_cast<int>(m_size), static_cast<int>(newSize));
+
+	for (int i = 0; i < copySize; i++)
+	{
+		copy[i] = m_data[i];
+	}
+
+	for (int j = copySize; j < newSize; j++)
+	{
+		copy[j] = T{};
+	}
+
+	delete[] m_data;
+	m_size = newSize;
+	m_data = copy;
+}
+
+template<typename T>
+void DynamicArray<T>::push_back(T element)
+{
+	int newSize = m_size + 1;
+	this->setSize(newSize);
+	m_data[m_size - 1] = element;
+}
+
+template<typename T>
+DynamicArray<T>::~DynamicArray()
+{
+	delete[] m_data;
+}
+
+template<typename T>
+T& DynamicArray<T>::operator[](std::size_t index)
+{
+	return m_data[index];
+}
+
+template<typename T>
+void DynamicArray<T>::clear()
+{
+	delete[] m_data;
+	m_size = 0;
+	m_data = new int[m_size];
+}
+
+template<typename T>
+bool DynamicArray<T>::operator==(const DynamicArray<T>& other)
+{
+	if (this->m_size != other.m_size)
+		return false;
+
+	for (int i = 0; i < m_size; i++)
+	{
+		if (this->m_data[i] != other.m_data[i])
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+template<typename T>
+bool DynamicArray<T>::operator!=(const DynamicArray<T>& other)
+{
+	return !(*this == other);
+}
+
+template<typename T>
+std::size_t DynamicArray<T>::getSize() const
+{
+	return m_size;
+}
--- HM23_DynamicArray/HM_23_main.cpp
+++ HM23_DynamicArray/HM_23_main.cpp
@@ -3,8 +3,9 @@
 
 int main()
 {
-	DynamicIntArray array1;
-	DynamicIntArray array2(5);
+	DynamicArray<int> array1;
+	DynamicArray<int> array2(5);
+
 	std::cout << "Array 1 size: " << array1.getSize() << std::endl;
 	std::cout << "Array 2 size: " << array2.getSize() << std::endl;
 	for (int i = 0; i < array2.getSize(); i++)
@@ -15,7 +16,7 @@ int main()
 	std::cout << "Second element of array 2 is : " << array2[2] << std::endl;
 	array2.clear();
 	std::cout << "Array 2 size after .clear() : " << array2.getSize() << std::endl;
-	
+
 	if (array1 == array2)
 	{
 		std::cout << "true\n";
@@ -24,8 +25,8 @@ int main()
 	{
 		std::cout << "false\n";
 	}
-	DynamicIntArray array3(5);
-	DynamicIntArray array4(4);
+	DynamicArray<int> array3(5);
+	DynamicArray<int> array4(4);
 	array3[2] = 13;
 	array4[2] = 13;
 	if (array3 == array4)
@@ -53,4 +54,14 @@ int main()
 	std::cout << "Array 4 size after .setSize(3) : " << array4.getSize() << std::endl;
 	array4.push_back(6);
 	std::cout << "Array 4 size after .push_back(5) : [" << array4.getSize() << "] 5th element is: [" << array4[6] << "]" << std::endl;
+
+	// HM23
+	DynamicArray<std::string> str1;
+	DynamicArray<std::string> str2(5);
+
+	std::cout << "\n\n\nstring array 1 size: " << str1.getSize() << std::endl;
+	std::cout << "string array 2 size: " << str2.getSize() << std::endl;
+	str1.push_back("abc");
+	std::cout << "zero element of str1 after push_back: " << str1[0];
+	str1 = str2;
 }
